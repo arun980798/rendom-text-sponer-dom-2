c425f6167eb05c564f6a201897c7585f01dd8efd
arun980798 <arunrajput88176894@gmail.com>
2025-11-18 20:51:28 +0530

    task2 done

---
+++ script.js
@@ -0,0 +1,24 @@
+const box = document.querySelector("main");
+const button = document.querySelector("button");
+const randomWords = [
+  "apple", "sunshine", "keyboard", "mountain", "river", "planet", "javascript", "rocket", "mirror", "galaxy", "energy", "forest", "diamond", "ocean", "camera"
+];
+
+
+
+
+
+
+button.addEventListener("click", () => {
+  let a = Math.floor(Math.random() * 16);
+  let b = Math.floor(Math.random() * 100);
+  let c = Math.floor(Math.random() * 100);
+  let h3 = document.createElement("h1");
+  h3.style.color= "white"
+  h3.style.position = "absolute"
+  h3.style.top = b+"%";
+  h3.style.left = c+"%";
+  h3.innerHTML = randomWords[a]
+ box.appendChild(h3)
+
+})
