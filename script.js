fe5bf8f1df7ea5bb7447dc4ded550c11319c33d7
arun980798 <arunrajput88176894@gmail.com>
2025-11-18 23:30:03 +0530

    full done

--- script.js
+++ script.js
@@ -5,20 +5,24 @@ const randomWords = [
 ];
 
 
-
-
-
-
 button.addEventListener("click", () => {
   let a = Math.floor(Math.random() * 16);
   let b = Math.floor(Math.random() * 100);
   let c = Math.floor(Math.random() * 100);
+  let d = Math.floor(Math.random() * 360);
+
   let h3 = document.createElement("h1");
-  h3.style.color= "white"
-  h3.style.position = "absolute"
+  h3.style.color= "white";
+  h3.style.position = "absolute";
   h3.style.top = b+"%";
   h3.style.left = c+"%";
-  h3.innerHTML = randomWords[a]
- box.appendChild(h3)
+  h3.style.rotate =d+"deg";
+  h3.innerHTML = randomWords[a];
+setTimeout(()=>{
+  box.appendChild(h3);
+},800)
 
+ setTimeout(()=>{
+  h3.style.display ="none";
+ },b+"000")
 })
